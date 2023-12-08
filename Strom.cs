f8842877a23e7684571b546c279066bd0ecfb750
alexsikur <alexander.sikur@spsjm.eu>
2023-12-08 13:06:46 +0100

    Create Strom.cs

---
+++ Strom.cs
@@ -0,0 +1,41 @@
+﻿using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Strom
+    {
+        public Strom() { }
+        ~Strom() { }
+        public double Volume
+        {
+            get {return Math.PI * (r * r) * h / 3; }
+        }
+        protected double r;
+        private double h;
+    }
+    class Baobab : Strom
+    {
+        public double Volume
+        {
+            get { return Math.PI * (r * r) * h; }
+        }
+        protected double r;
+        private double h;
+    }
+    class ihlicnan : Strom
+    {
+        public double Volume
+        {
+            get { return Math.PI * (r * r) * h; }
+        }
+        protected double r;
+        private double h;
+    }
+    
+}
