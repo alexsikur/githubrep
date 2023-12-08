a4560f02429b208316d8b456eed530aff9d9051a
alexsikur <alexander.sikur@spsjm.eu>
2023-12-08 13:05:57 +0100

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,26 +11,31 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Strom strom1= new Strom();
+
+
             double r;
             double h;
             double v = 0;
             double c = 0;
-            
 
-            for (int i = 0; i < 4; i++)
+
+            for (int i = 0; i < 5; i++)
             {
+
                 Console.WriteLine("Zadaj radius:");
                 r = double.Parse(Console.ReadLine());
                 Console.WriteLine("Zadaj vysku:");
                 h = double.Parse(Console.ReadLine());
                 v = Math.PI * (r * r) * h / 3;
                 c = v + c;
-                Console.WriteLine(v);
-                
+                Console.WriteLine("objem je:" + v);
+
             }
-            Console.WriteLine(c);
+            Console.WriteLine("celkovy objem: " + c);
             Console.ReadLine();
 
         }
+
     }
 }
