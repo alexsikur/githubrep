6e68c18335bf84f24f5247692e0bfd89a073233a
alexsikur <alexander.sikur@spsjm.eu>
2023-12-15 13:07:50 +0100

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -9,9 +9,47 @@ namespace ConsoleApp2
 {
     internal class Program
     {
+        interface IOrganizmus
+        {
+            void rastie();
+        }
+        class Strom : IOrganizmus
+        {
+            public virtual void rastie()
+            {
+
+            }
+            public Strom() { }
+            ~Strom() { Console.WriteLine("destructor"); }
+            public virtual double Volume
+            {
+                get { return Math.PI * (r * r) * h / 3; }
+            }
+            public double r;
+            public double h;
+        }
+       //dedicnost, enkapsulacia, polyformizmus
+       //ako je v kode dedicnost - (class Baobab:Leaf)
+       //objekt je instanciou triedy/ je hmotny 
+       //trieda je predpis ako sa ma objekt vytvarat
+       //polyformizmus -  override
+       //enkapsulacia - public, private, protected
+       //interface - je specialny druh triedy ktora ma deklaraciu a nie definiciu
+       //            
+        class Baobab : Strom
+        {
+            public Baobab() { }
+            ~Baobab() { }
+
+            public override double Volume
+            {
+                get { return Math.PI * (r * r) * h; }
+            }
+        }
         static void Main(string[] args)
         {
             Strom strom1= new Strom();
+            Baobab strom2 = new Baobab();
 
 
             double r;
@@ -20,7 +58,7 @@ namespace ConsoleApp2
             double c = 0;
 
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 2; i++)
             {
 
                 Console.WriteLine("Zadaj radius:");
@@ -33,9 +71,12 @@ namespace ConsoleApp2
 
             }
             Console.WriteLine("celkovy objem: " + c);
+            Console.WriteLine();
             Console.ReadLine();
 
         }
 
+
     }
+
 }
